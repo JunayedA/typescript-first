7cab45db4d9486527ab1fd4f758e6c959ad9f637
JunayedAhmed <junayedahmed11099@gmail.com>
2021-09-12 00:19:12 +0600

    typescript

---
+++ first.ts
@@ -0,0 +1,8 @@
+/*
+ let number = 324324;
+let string = 'helo';
+let boolean = true; 
+*/
+let money:number = 5;
+let honey:string = 'ohh my honey';
+let kony:boolean = true;
---
+++ js/first.js
@@ -0,0 +1,4 @@
+"use strict";
+const myNumber = 5;
+const myName = 'Junayed';
+console.log('hello from Typescript', myNumber, myName);

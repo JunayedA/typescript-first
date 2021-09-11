b17df919445be9ff9ba9c84a97c68414b060ccab
JunayedAhmed <junayedahmed11099@gmail.com>
2021-09-12 00:54:10 +0600

    function type

--- first.ts
+++ first.ts
@@ -6,3 +6,26 @@ let boolean = true;
 let money:number = 5;
 let honey:string = 'ohh my honey';
 let kony:boolean = true;
+
+
+function add (first:number,second:number):number{
+    const result = first+second;
+    return result;
+}
+
+const output: number = add(34,78);
+
+function doubleConsole(number:number):void{
+    console.log(2*number);
+}
+
+
+//multi purpose
+/* function add (first:number|string,second:number|string):number|string{
+    const result:number|string = first+second;
+    return result;
+}
+
+const output: number = add(34,78);
+const fullName= add('adam','sendla');
+ */

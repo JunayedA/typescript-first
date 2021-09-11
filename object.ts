f79c9daab870767b046417a6b4f8051de0aa24b7
JunayedAhmed <junayedahmed11099@gmail.com>
2021-09-12 02:12:57 +0600

    object type

---
+++ object.ts
@@ -0,0 +1,20 @@
+const city:string = 'Dahaka';
+const fruits:string [] = ['mango','banana','apple'];
+
+
+type person ={
+    name:string,
+    age:number,
+    salary:number
+};
+const student:person = {
+    name:'Junayed Ahmed',
+    age:23,
+    salary:45000
+}
+
+const employee:{name:string,age:number,salary:number} ={
+    name:'solayman',
+    age:29,
+    salary:100000
+}
